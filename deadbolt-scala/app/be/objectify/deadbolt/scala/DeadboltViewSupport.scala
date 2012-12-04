91985e19dce369ca431776cd44d8853330eb3ba0
Steve Chaloner <github-NOSPAM@objectify.be>
2012-12-04 22:19:31 +0100

    added dynamic tag support to deadbolt-scala

--- deadbolt-scala/app/be/objectify/deadbolt/scala/DeadboltViewSupport.scala
+++ deadbolt-scala/app/be/objectify/deadbolt/scala/DeadboltViewSupport.scala
@@ -32,4 +32,27 @@ object DeadboltViewSupport {
     else false
   }
 
+  /**
+   * Used for dynamic tags in the template.
+   *
+   * @param name the name of the resource
+   * @param meta meta information on the resource
+   * @return true if the view can be accessed, otherwise false
+   */
+  def viewDynamic(name: String,
+                  meta: String,
+                  deadboltHandler: DeadboltHandler,
+                  request: Request[Any]): Boolean =
+  {
+    val resourceHandler: DynamicResourceHandler = deadboltHandler.getDynamicResourceHandler(request)
+    if (resourceHandler == null)
+    {
+      throw new RuntimeException("A dynamic resource is specified but no dynamic resource handler is provided")
+    }
+    else
+    {
+      if (resourceHandler.isAllowed(name, meta, deadboltHandler, request)) true
+      else false
+    }
+  }
 }
